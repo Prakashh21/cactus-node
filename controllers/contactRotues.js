76ae6f02c4d8ddcca2138a692d52a7c573b5567f
Prakash <prakashope@gmail.com>
2023-05-23 23:59:45 +0530

    async request handler , error middleware

--- controllers/contactRotues.js
+++ controllers/contactRotues.js
@@ -1,23 +1,30 @@
-const getAllContacts = (req, res) => {
+const asyncHandler = require("express-async-handler");
+const  getAllContacts = async (req, res) => {
     return res.status(200).send({message: "all contacts"})
 }
 
-const createContact = (req , res) => {
+const createContact = asyncHandler(async (req , res) => {
     console.log(req.body)
+
+    const {name , email , phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory !")
+    }
     return res.status(200).send({message: "create contact"})
-}
+})
 
-const getContactById = (req , res) => {
+const getContactById = asyncHandler(async (req , res) => {
     return res.status(200).send({message: `get contact by of id ${req.params.id}`})
-}
+})
 
-const updateContactsById = (req ,res) => {
+const updateContactsById = asyncHandler(async (req ,res) => {
     return res.status(200).send({message: `update contact by id ${req.params.id}`})
-}
+})
 
-const deleteContactById = (req , res) => {
+const deleteContactById = asyncHandler(async (req , res) => {
     return res.status(200).send({message: `delete contact by id ${req.params.id}`})
-}
+})
 
 
 
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const router = require('./routes/contactRoutes');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express()
 
 // app.get("/",(req , res) => res.send("hello express"));
 app.use(express.json());
 app.use("/api/", router);
+app.use(errorHandler)
+console.log(process.env.PORT)
+
+const port = process.env.PORT || 5500;
 
 
-const port = process.env.port || 5500;
-
-
-app.listen(port, () => console.log("listening on port 5500"))
+app.listen(port, () => console.log("listening on port ",port))
